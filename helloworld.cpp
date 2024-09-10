6047e46cf6e5fb83a70df07f04eb8aa0dff5cbcb
MatthewMartin93 <161871982+MatthewMartin93@users.noreply.github.com>
2024-09-10 09:14:08 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ int a;
+ float b;
+ bool c;
+ char d;
+ std::string e;
+int main()
+{
+    a = 8934;
+    b = 32.78932;
+    c = false;
+    d = 'd';
+    e = "Madden NFL 25 Ultimate Team";
+    std::cout << "Hello World" << std::endl;
+    std::cout << a + b << std::endl;
+    std::cout << d << " " <<  c << " " 
+    << e << std::endl;
+}
